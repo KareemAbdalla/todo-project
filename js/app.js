584fed3b40110b88da68413be4ec6c0098eeb2ca
karim1996 <kareemabdallah9627@gmail.com>
2022-04-21 06:45:37 +0300

    updating task 06

--- js/app.js
+++ js/app.js
@@ -1,12 +1,37 @@
 
-var name = prompt("What is your name?")
-var gender = prompt("Male or Female?")
-var age = prompt("How old are you?")
+let name = prompt("What is your name?")
+let gender = prompt("Male or Female?")
+let age = prompt("How old are you?")
+
+// yes or no questions 
+let job = prompt("Do You Have A Job?")
+let mstatus = prompt("Are You Married?")
+let  usage = prompt("Are You Using Your Phone To Enter This Website?")
+
+if(job == ""){
+    job = "invalid"
+ //   console.log("job is " + job)
+}
+
+if(mstatus == ""){
+    mstatus = "invalid"
+ //   console.log("mstatus is " + mstatus)
+}
+
+if(usage == ""){
+    usage = "invalid"
+ //   console.log("usage is" + usage)
+}
+
+const answers = [job, mstatus, usage];
+for(let i = 0 ; i < answers.length ; i++){
+    console.log(answers[i])
+}
 
 if(age <= 0)
 {alert("The Age Must Be More Than Zero")}
 
- var confirmMsg = confirm("Do You Want To Skip The Welcoming Message?")
+ var confirmMsg = confirm("Click On Cancel To Skip The Welcoming Message?")
 
     if(gender == 'M' || gender == 'm' && confirmMsg == true)
     {
